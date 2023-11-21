70b0310b480d8c8c4b0c208013f1cae0ef3083ce
Alex Hewitt <alexhewitt.j@gmail.com>
2023-11-21 15:28:08 -0600

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import trollFace from '/assets/troll-face.svg'
+
+function Header() {
+    return (
+        <header className='header'>
+            <div className='header-left'>
+                <img src={trollFace} className='header-img'/>
+                <h1 className='white'>Meme Generator</h1>
+            </div>
+            <div className='header-right'>
+                <h2>React Course - Project 3</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header
