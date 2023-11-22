8db8047a7fff6f4fa14130c5ad93644880fe48cd
Alex Hewitt <alexhewitt.j@gmail.com>
2023-11-22 15:21:47 -0600

    add image url state and styling

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,24 +2,29 @@ import React from 'react'
 import data from '../memesData'
 
 function Meme() {
+    const [memeImage, getMemeImage] = React.useState("")
+
     function getRandomImage() {
         const dataLength = data.data.memes.length
         const memesArray = data.data.memes
         const randomNumber = Math.floor(Math.random() * dataLength)
         const url = memesArray[randomNumber].url
-        console.log(url)
+        getMemeImage(prevUrl => url)
     }
 
     return (
-        <div className='form'>
-            <div className='form-inputs'>
-                <input type='text' className='input-styling'></input>
-                <input type='text' className='input-styling'></input>
+        <main className='main-content'>
+            <div className='form'>
+                <div className='form-inputs'>
+                    <input type='text' className='input-styling'></input>
+                    <input type='text' className='input-styling'></input>
+                </div>
+                <button onClick={getRandomImage} className='form-button'>
+                Get a new meme image
+                </button>
             </div>
-            <button onClick={getRandomImage} className='form-button'>
-            Get a new meme image
-            </button>
-        </div>
+            <img src={memeImage} className='meme-img'/>
+        </main>
     )
 }
 
